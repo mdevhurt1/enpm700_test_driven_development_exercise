fb2f45c7bb9ca6a963da5b16602ebe49cec0c4de
Marcus <mdevhurt@gmail.com>
2025-10-06 17:11:19 +0000

    Added doxygen comments for public functions of PID Controller class.

--- libs/pid_controller/pid_controller.hpp
+++ libs/pid_controller/pid_controller.hpp
@@ -14,14 +14,33 @@
 
 class PIDController {
 public:
+    /**
+     * @brief Construct a new PIDController object
+     * 
+     * @param kp 
+     * @param ki 
+     * @param kd 
+     */
     PIDController(double kp, double ki, double kd);
 
+    /**
+     * @brief Compute the PID controller output
+     * 
+     * @param setpoint - desired value
+     * @param measured_value - current value
+     * @param dt - timestep
+     * @return double 
+     */
     double compute(double setpoint, double measured_value, double dt);
 
     double getKp() const;
     double getKi() const;
     double getKd() const;
 
+    /**
+     * @brief Reset the PID controller state (integral and previous error)
+     * 
+     */
     void reset();
 
 private:
