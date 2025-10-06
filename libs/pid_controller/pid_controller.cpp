1883023ba7824ed5151714e53b821c448bf0c7cf
Marcus <mdevhurt@gmail.com>
2025-10-06 15:53:28 +0000

    Added file structure for PID Controller class.

---
+++ libs/pid_controller/pid_controller.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file pid_controller.cpp
+ * @author Marcus Hurt (mhurt@umd.edu)
+ * @brief PID controller class implementation
+ * @version 0.1
+ * @date 2025-10-06
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include "pid_controller.hpp"
---
+++ libs/pid_controller/pid_controller.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file pid_controller.hpp
+ * @author Marcus Hurt (mhurt@umd.edu)
+ * @brief PID controller class definition
+ * @version 0.1
+ * @date 2025-10-06
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+ #pragma once
