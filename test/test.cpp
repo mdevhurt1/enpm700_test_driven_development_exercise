dcf9e817438a97c7c16d9dc5c24d448086eed16e
Madhav2133 <vmadhav@umd.edu>
2025-10-15 16:05:15 -0400

    Formatting and comments using clang-format

--- test/test.cpp
+++ test/test.cpp
@@ -7,4 +7,6 @@ TEST(dummy_test, this_should_pass) { EXPECT_EQ(1, 1); }
 
 TEST(dummy_test, this_should_pass_too) { EXPECT_EQ(my_function1(3), 3); }
 
-TEST(dummy_test, this_will_also_pass) { EXPECT_NEAR(my_function2(3.2), 3.2, 3.2); }
+TEST(dummy_test, this_will_also_pass) {
+  EXPECT_NEAR(my_function2(3.2), 3.2, 3.2);
+}
